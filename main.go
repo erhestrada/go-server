754e13100dc823b8950c83df2e0d503aa23398c0
Erik <180784081+erhetrada@users.noreply.github.com>
2024-09-30 13:25:27 -0400

    initialize checkHits and display on /metrics; wrap main page handler in hits counter

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
 
 func checkReadiness(w http.ResponseWriter, req *http.Request) {
@@ -11,11 +12,31 @@ func checkReadiness(w http.ResponseWriter, req *http.Request) {
 
 }
 
+func checkHits(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	cfg := &apiConfig{fileserverHits: atomic.Int32{}}
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	//serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	serveMux.Handle("/app/assets/catpfp.jpg", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	serveMux.HandleFunc("/healthz", checkReadiness)
+	serveMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("/metrics", checkHits)
+
 	server := &http.Server{Addr: ":8080",
 		Handler: serveMux}
 	server.ListenAndServe()
