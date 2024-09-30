3e27bf4d05afeb4fe254b39d7d06e2e48db1a917
Erik <180784081+erhetrada@users.noreply.github.com>
2024-09-30 10:01:53 -0400

    made checkReadiness to handle /healthz

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
+
+func checkReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
+}
 
 func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/assets/catpfp.jpg", http.FileServer(http.Dir(".")))
+	serveMux.HandleFunc("/healthz", checkReadiness)
 	server := &http.Server{Addr: ":8080",
 		Handler: serveMux}
 	server.ListenAndServe()
