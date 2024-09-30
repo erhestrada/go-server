9ad3f671a5a7a1f1e7654a1a0a22f47846fdee6c
Erik <180784081+erhetrada@users.noreply.github.com>
2024-09-30 10:15:34 -0400

    change path from / to /app/ and use StripPrefix for URLs that use http.FileServer

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ func checkReadiness(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
-	serveMux.Handle("/assets/catpfp.jpg", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.Handle("/app/assets/catpfp.jpg", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	serveMux.HandleFunc("/healthz", checkReadiness)
 	server := &http.Server{Addr: ":8080",
 		Handler: serveMux}
